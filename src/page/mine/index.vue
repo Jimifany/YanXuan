8ca2141cd296c161b514151cbf0173130088b1f9
LXY <L3412897394@163.com>
2019-09-24 08:34:39 +0800

    mine部分排版

--- src/page/mine/index.vue
+++ src/page/mine/index.vue
@@ -1,13 +1,37 @@
 <template>
-    <div>
-        1
+  <div>
+    <div class="minePage">
+      <div class="userMsgWrap">
+        <div class="userLogo">
+          <img src="http://yanxuan.nosdn.127.net/8945ae63d940cc42406c3f67019c5cb6.png" alt />
+        </div>
+        <div class="userMsgs">
+          <div>15645655675</div>
+          <div>普通用户</div>
+        </div>
+      </div>
+      <div class="userPower">
+        <div class="Item">
+          <i class="icon iconfont icon-7tian"></i>
+          <div>我的收藏</div>
+        </div>
+        <div class="Item">
+          <i class="icon iconfont icon-7tian"></i>
+          <div>我的收藏</div>
+        </div>
+        <div class="Item">
+          <i class="icon iconfont icon-7tian"></i>
+          <div>我的收藏</div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
 <style>
-    @import url('./index.css');
+/* @import url('./index.css'); */
+@import url("./index.scss");
+@import url("../../static/fonts2/iconfont.css");
 </style>
