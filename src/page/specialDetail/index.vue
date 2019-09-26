a604f8e918903cd9410ebac665152c2875ede196
LXY <L3412897394@163.com>
2019-09-26 09:03:18 +0800

    返回上一页面

--- src/page/specialDetail/index.vue
+++ src/page/specialDetail/index.vue
@@ -1,27 +1,23 @@
 <template>
   <div>
-    <div class="noTabPageContent" >
-       <div class="header">
-      <div class="left">&lt;</div>
-      <div class="mi">{{detailList.title}}</div>
-      <div class="right"></div>
-    </div>
-    <div class="DetailBox">
-      <!-- <div class="Pic" v-for="(value,key,index) in detailList" :key="index"> -->
+    <div class="noTabPageContent">
+      <div class="header">
+        <div class="left" @click="Back">&lt;</div>
+        <div class="mi">{{detailList.title}}</div>
+        <div class="right"></div>
+      </div>
+      <div class="DetailBox">
+        <!-- <div class="Pic" v-for="(value,key,index) in detailList" :key="index"> -->
         <div class="IMG" v-html="detailList.content"></div>
         <!-- </div> -->
         <!-- <img :src="detailList.item_pic_url" alt=""> -->
-
+      </div>
     </div>
-   
-    </div>
-   
- 
   </div>
 </template>
 <script>
 import { mapState, mapActions } from "vuex";
-import { specialDetail } from '../../server';
+import { specialDetail } from "../../server";
 export default {
   data() {
     return {};
@@ -30,19 +26,19 @@ export default {
     ...mapState("specialDetail", ["detailList"])
   },
   methods: {
-    ...mapActions("specialDetail", ["getdetailFn"])
-    
+    ...mapActions("specialDetail", ["getdetailFn"]),
+    Back() {
+      console.log(1111);
+     this.$router.go(-1)
+    }
   },
   created() {
     let id = this.$route.query.newId;
     console.log(id);
     this.getdetailFn({ id: id });
     console.log(this.$route.query.newId);
-    
   },
-  mounted() {
-    
-  }
+  mounted() {}
 };
 </script>
 
